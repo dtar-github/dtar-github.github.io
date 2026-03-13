dc0e38f42b1f134f6b77ad1dd355a4214dfec697
Devin <devin.t.ramirez@gmail.com>
2026-03-13 11:49:03 -0500

    Fixed darkmode issues in iframes

--- darkmode.js
+++ darkmode.js
@@ -1,15 +1,21 @@
-const toggle = document.getElementById("dark");
-const body = document.body;
-if (localStorage.getItem("darkMode") === "enabled") {
-    body.classList.add("dark");
-    toggle.checked = true;
+const params = new URLSearchParams(window.location.search)
+nonav = (params.get("nonav") == "true")
+if (nonav) {
+    document.querySelectorAll("nav").forEach(nav => nav.remove())
+}
+
+const toggle = document.getElementById("dark")
+const body = document.body
+if (localStorage.getItem("darkMode") === "enabled" && !nonav) {
+    body.classList.add("dark")
+    toggle.checked = true
 }
 toggle.addEventListener("change", () => {
     if (toggle.checked) {
-        body.classList.add("dark");
-        localStorage.setItem("darkMode", "enabled");
+        body.classList.add("dark")
+        localStorage.setItem("darkMode", "enabled")
     } else {
-        body.classList.remove("dark");
-        localStorage.setItem("darkMode", "disabled");
+        body.classList.remove("dark")
+        localStorage.setItem("darkMode", "disabled")
     }
-});
+})
--- nonav.js
+++
@@ -1,4 +0,0 @@
-const params = new URLSearchParams(window.location.search)
-if (params.get("nonav")=="true") {
-    document.querySelectorAll("nav").forEach(nav => nav.remove())
-}
