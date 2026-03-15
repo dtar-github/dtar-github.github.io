e784f2c1aa8f0e31f148935be2107f2e32d18e5b
Devin <devin.t.ramirez@gmail.com>
2026-03-15 17:15:27 -0500

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,12 +1,12 @@
 const params = new URLSearchParams(window.location.search)
+dark = params.get("dark")
 nonav = (params.get("nonav") == "true")
 if (nonav) {
     document.querySelectorAll("nav").forEach(nav => nav.remove())
 }
-
 const toggle = document.getElementById("dark")
 const body = document.body
-if (localStorage.getItem("darkMode") === "enabled" && !nonav) {
+if (localStorage.getItem("darkMode") === "enabled" && !nonav && dark != "false" || dark == "true") {
     body.classList.add("dark")
     toggle.checked = true
 }
